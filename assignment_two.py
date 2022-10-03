6f9bed0c1580f1e963448d36081e5fbb837674e0
Joshg12 <112645058+Joshg12@users.noreply.github.com>
2022-10-03 09:46:21 -0400

    Project 2

---
+++ assignment_two.py
@@ -0,0 +1,12 @@
+name = input("What's your name?")
+
+print ("That is a nice name",name)
+
+
+artist = str.lower(input("Who is your favorite musical artist?"))
+
+if "taylor" in artist:
+    print("Taylor is awesome!")
+
+else:
+    print("Well,okay, but I like Taylor Swift")
